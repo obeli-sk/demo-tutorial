36d3885a318579c371e52fcc1b2c413356ae1c62
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-11-19 20:56:39 +0100

    chore: Generate workflow from `fibo` template

---
+++ workflow-tutorial/src/lib.rs
@@ -0,0 +1,33 @@
+use exports::template_fibo::workflow::fibo_workflow_ifc::Guest;
+use obelisk::workflow::workflow_support::{new_join_set_generated, ClosingStrategy};
+use template_fibo::{
+    activity::fibo_activity_ifc::fibo as fibo_activity,
+    activity_obelisk_ext::fibo_activity_ifc::{fibo_await_next, fibo_submit},
+};
+use wit_bindgen::generate;
+
+generate!({ generate_all });
+struct Component;
+export!(Component);
+
+impl Guest for Component {
+    fn fiboa(n: u8, iterations: u32) -> Result<u64, ()> {
+        let mut last = 0;
+        for _ in 0..iterations {
+            last = fibo_activity(n).unwrap();
+        }
+        Ok(last)
+    }
+
+    fn fiboa_concurrent(n: u8, iterations: u32) -> Result<u64, ()> {
+        let join_set = new_join_set_generated(ClosingStrategy::Complete);
+        for _ in 0..iterations {
+            fibo_submit(&join_set, n);
+        }
+        let mut last = 0;
+        for _ in 0..iterations {
+            last = fibo_await_next(&join_set).unwrap().1.unwrap();
+        }
+        Ok(last)
+    }
+}
