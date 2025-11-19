162a9078fa3756c1d179691e9f30cafc456a4030
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-11-19 18:36:13 +0100

    feat: Add file creation to `activity-sleepy`

--- activity-sleepy/src/lib.rs
+++ activity-sleepy/src/lib.rs
@@ -1,6 +1,5 @@
-use std::time::Duration;
-
 use exports::tutorial::activity::activity_sleepy::Guest;
+use std::time::Duration;
 use wit_bindgen::generate;
 
 generate!({ generate_all });
@@ -10,6 +9,10 @@ export!(Component);
 impl Guest for Component {
     fn step(idx: u64, sleep_millis: u64) -> Result<u64, ()> {
         std::thread::sleep(Duration::from_millis(sleep_millis));
+        let path = format!("file-{idx}.txt");
+        std::fs::File::create(path)
+            .inspect_err(|err| eprintln!("{err:?}"))
+            .map_err(|_| ())?;
         Ok(idx)
     }
 }
